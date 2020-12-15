87b184573069e3304d317ef2cf856baa4fe9e71e
AlenaAlyona <izakson91@gmail.com>
2020-12-15 11:34:30 +0100

    added save and cancel icons in edit mode

--- src/components/ToDoItem.tsx
+++ src/components/ToDoItem.tsx
@@ -1,47 +1,37 @@
 import React, { useState } from "react";
 import * as AiIcons from "react-icons/ai";
 import * as MdIcons from "react-icons/md";
+import * as FiIcons from "react-icons/fi";
+import * as GiIcons from "react-icons/gi";
 import { Props } from "../model";
 import "./toDoItem.css";
 
 function ToDoItem(props: Props) {
   const [editMode, setEditMode] = useState(false);
+  // const [itemText, setItemText] = useState(props.item.text);
 
   const editItem = () => {
     setEditMode(true);
   };
+
+  const handleEdit = () => {};
+
   if (editMode) {
     return (
       <div>
-        {props.item.complete ? (
-          <label className="container">
-            <input type="checkbox" onClick={props.toggleDone} />
-            <span className="checkmark"></span>
-            <span style={{ textDecoration: "line-through" }}>
-              <input
-                type="text"
-                value={props.item.text}
-                name="editVal"
-                id="editVal"
-              />
-            </span>{" "}
-            <AiIcons.AiFillDelete onClick={props.onDelete} />
-            <MdIcons.MdModeEdit />
-          </label>
-        ) : (
-          <label className="container">
-            <input type="checkbox" onClick={props.toggleDone} />
-            <span className="checkmark"></span>
-            <input
-              type="text"
-              value={props.item.text}
-              name="editVal"
-              id="editVal"
-            />
-            <AiIcons.AiFillDelete onClick={props.onDelete} />
-            <MdIcons.MdModeEdit />
-          </label>
-        )}
+        <label className="container">
+          <input type="checkbox" onClick={props.toggleDone} disabled />
+          <span className="checkmark"></span>
+        </label>
+        <input
+          type="text"
+          value={props.item.text}
+          name="editVal"
+          id="editVal"
+          onChange={handleEdit}
+        />
+        <FiIcons.FiSave />
+        <GiIcons.GiCancel />
       </div>
     );
   } else {
@@ -55,7 +45,7 @@ function ToDoItem(props: Props) {
               {props.item.text}
             </span>{" "}
             <AiIcons.AiFillDelete onClick={props.onDelete} />
-            <MdIcons.MdModeEdit />
+            <MdIcons.MdModeEdit onClick={editItem} />
           </label>
         ) : (
           <label className="container">
--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -7,14 +7,12 @@ export default function ToDoList(props: PropsList) {
     <div>
       {props.list.map((item) => {
         return (
-          <div>
-            <ToDoItem
-              key={item.id}
-              item={item}
-              toggleDone={() => props.toggle(item.id)}
-              onDelete={() => props.handleDelete(item.id)}
-            />
-          </div>
+          <ToDoItem
+            key={item.id}
+            item={item}
+            toggleDone={() => props.toggle(item.id)}
+            onDelete={() => props.handleDelete(item.id)}
+          />
         );
       })}
     </div>
